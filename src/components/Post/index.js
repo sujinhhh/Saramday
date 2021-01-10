6ff434eb0379fba302ba160d401c60b95ec48c35
이수진_세부내용 작성 <email@sujinmail.com>
2021-01-09 22:16:56 -0500

    video UI

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,18 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import styles from './styles';
 import Video from 'react-native-video';
+
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const Post = () => {
   const [paused, setPaused] = useState(false);
@@ -23,6 +34,45 @@ const Post = () => {
           paused={paused}
         />
       </TouchableWithoutFeedback>
+      <View style={styles.uiContainer}>
+        <View style={styles.rightContainer}>
+          <View>
+            <Image
+              style={styles.profilePictureContainer}
+              source={require('../../assets/chan.png')}
+            />
+          </View>
+          <View style={styles.iconContainer}>
+            <View>
+              <AntDesign name={'heart'} size={40} color="white" />
+            </View>
+            <Text style={styles.stateLebel}>Side</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <View>
+              <FontAwesome name={'commenting'} size={40} color="white" />
+            </View>
+
+            <Text style={styles.stateLebel}>Side</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <View>
+              <Fontisto name={'share-a'} size={35} color="white" />
+            </View>
+            <Text style={styles.stateLebel}>Side</Text>
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Text style={styles.handle}>User name</Text>
+          <Text style={styles.decription}>Description</Text>
+          <View style={styles.songRow}>
+            {/* icon */}
+            <Entypo name={'beamed-note'} size={24} color="white" />
+            {/* name */}
+            <Text style={styles.songName}>He will make us strong</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -3,7 +3,7 @@ import {StyleSheet, Dimensions} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height - 20,
   },
 
   video: {
@@ -12,6 +12,59 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     right: 0,
+  },
+  uiContainer: {
+    height: '100%',
+    justifyContent: 'flex-end',
+    marginRight: 10,
+  },
+  rightContainer: {
+    alignSelf: 'flex-end',
+    height: 300,
+    justifyContent: 'space-between',
+  },
+  bottomContainer: {
+    padding: 10,
+  },
+  handle: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 5,
+  },
+  decription: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '500',
+    paddingBottom: 5,
+  },
+
+  songRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  songName: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  profilePictureContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#fff',
+    marginBottom: 20,
+  },
+  iconContainer: {
+    alignItems: 'center',
+  },
+
+  stateLebel: {
+    color: '#fff',
+    paddingVertical: 5,
+    fontWeight: '600',
+    marginTop: 5,
   },
 });
 
